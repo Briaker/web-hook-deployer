49dd12dcc9fb51c6436ffc0e3882728e5877e1d9
Briaker <mail@brianbaker.ca>
2017-03-09 19:32:09 -0500

    cleaned up code format

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ RELEASE_DIR='/var/www/hackeryou';
 APP_DIR='/var/www/hackeryou/web-hook-demo';
 
 # Fetch Latest Code
-[ -d $RELEASE_DIR ] || mkdir $RELEASE_DIR;
 cd $APP_DIR;
 git pull origin master;
 
+# Run Gulp Build Script
 gulp build
--- main.js
+++ main.js
@@ -9,19 +9,17 @@ var github = githubhook({
 console.log("Listening!");
 github.listen();
 
-
-
 github.on('*', function (event, repo, ref, data) {
     console.log("A webhook event has been fired!");
 });
 
-
-
 github.on('web-hook-demo:refs/heads/master', function (event, data) {
     console.log("The master branch of web-hook-demo has recieved a push event!");
+    
     var execOptions = {
         maxBuffer: 1024 * 1024 // Increase max buffer to 1mb
     };
+
     execFile('/var/www/hackeryou/deployer/deploy.sh', execOptions, function(error, stdout, stderr) {
         if( error ) {
             console.log(error);
